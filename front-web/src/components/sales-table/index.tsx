09e898edcc8f2bdc8de499296851d62fa99b51a2
Tiago Gomes <tiagogomes187@gmail.com>
2022-06-25 22:31:26 -0300

    Add SalesTable component

---
+++ front-web/src/components/sales-table/index.tsx
@@ -0,0 +1,80 @@
+import './styles.css';
+
+function SalesTable() {
+  return (
+    <div className="sales-table-container base-card">
+      <h3 className="sales-table-title">Vendas recentes</h3>
+      <table className="sales-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Data</th>
+            <th>Gênero</th>
+            <th>Categoria</th>
+            <th>Loja</th>
+            <th>Forma de pagamento</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>#341</td>
+            <td>07/11/2022</td>
+            <td>Feminino</td>
+            <td>Roupas e acessórios</td>
+            <td>São Paulo</td>
+            <td>Crédito</td>
+            <td>45.858,01</td>
+          </tr>
+          <tr>
+            <td>#341</td>
+            <td>07/11/2022</td>
+            <td>Feminino</td>
+            <td>Roupas e acessórios</td>
+            <td>São Paulo</td>
+            <td>Crédito</td>
+            <td>45.858,01</td>
+          </tr>
+          <tr>
+            <td>#341</td>
+            <td>07/11/2022</td>
+            <td>Feminino</td>
+            <td>Roupas e acessórios</td>
+            <td>São Paulo</td>
+            <td>Crédito</td>
+            <td>45.858,01</td>
+          </tr>
+          <tr>
+            <td>#341</td>
+            <td>07/11/2022</td>
+            <td>Feminino</td>
+            <td>Roupas e acessórios</td>
+            <td>São Paulo</td>
+            <td>Crédito</td>
+            <td>45.858,01</td>
+          </tr>
+          <tr>
+            <td>#341</td>
+            <td>07/11/2022</td>
+            <td>Feminino</td>
+            <td>Roupas e acessórios</td>
+            <td>São Paulo</td>
+            <td>Crédito</td>
+            <td>45.858,01</td>
+          </tr>
+          <tr>
+            <td>#341</td>
+            <td>07/11/2022</td>
+            <td>Feminino</td>
+            <td>Roupas e acessórios</td>
+            <td>São Paulo</td>
+            <td>Crédito</td>
+            <td>45.858,01</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default SalesTable;
